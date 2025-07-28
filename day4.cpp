03730ce5f4a3bc43ff0394b3257a1f3df3453908
Raunak Kumar Modi <raunakk046@gmail.com>
2025-07-28 09:04:22 +0530

    added day4

---
+++ day4.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[j] == target - nums[i]) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+int main(){
+    Solution sol;
+    vector<int>nums = {2,7,11,15};
+    int target = 9;
+    vector<int> result = sol.twoSum(nums,target);
+    if (!result.empty()){
+        cout << "indices: " << result[0] << ", " << result[1] << endl;
+    }else{
+        cout << "No solution found." << endl;
+    }
+    return 0;   
+}
